a679b02b961de1a09596ed1a6e36b42dfc545b29
Mark Conroy <mark@mark.ie>
2024-08-13 15:26:06 +0100

    adds menu

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("%cLet's do big things","color: #fff; background: #123456; font-size: 2rem; padding: 1rem;");
+console.log("%cthe Confident - Let's do big things","color: #fff; background: #123456; font-size: 2rem; padding: 1rem;");

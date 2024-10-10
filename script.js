f1cdc13a5c05a4e28d994f285efb8f9f97cb1abb
Mark Conroy <mark@mark.ie>
2024-10-10 20:48:45 +0100

    adds retainer service

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("%cthe Confident - Let's do big things","color: #fff; background: #123456; font-size: 2rem; padding: 1rem;");
+console.log("%cthe Confident - For those who demand the impossible","color: #fff; background: #123456; font-size: 2rem; padding: 1rem;");
